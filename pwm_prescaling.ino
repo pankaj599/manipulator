ccb31515d7969361f90dfaf8339e8a57ef1ef36d
pkvv <41447989+pkvv@users.noreply.github.com>
2019-01-24 00:49:02 +0530

    changing default pwm frequency
    in this we has changed the default pwm frequency using pwm prescaling

---
+++ pwm_prescaling.ino
@@ -0,0 +1,21 @@
+int dir1=4;                                  //direction pin of hercules
+int dir2=2;                                  // direction pin of hercules
+int pwm1=3;                                   // pwm pin of hercules
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(dir1,OUTPUT);
+  pinMode(dir2,OUTPUT);
+  pinMode(pwm1,OUTPUT);
+  TCCR2B = TCCR2B & 0b11111000 |0x02;         //pwm prescaling   
+ 
+}
+void loop()
+{
+   digitalWrite(dir1,0);
+   digitalWrite(dir2,1);
+   analogWrite(pwm1,127
+   );
+}
+
