a35659b253344d4faf7188836ebd8683a6253f0d
pkvv <41447989+pkvv@users.noreply.github.com>
2019-01-24 00:51:52 +0530

    changing defult frequency of pwm pin of arduino
    we change the default pwm frequency of arduino pin using <PWM.h> library

---
+++ pwm.ino
@@ -0,0 +1,26 @@
+#include <PWM.h>
+int32_t frequency=1000;                       //setting up a frequency
+
+//int dir1=4;
+int dir2=2;
+int pwm1=3;
+
+void setup()
+{
+
+  Serial.begin(9600);
+  InitTimersSafe();
+  SetPinFrequencySafe(pwm1,frequency);        //providing that frequency to pwm
+  //pinMode(dir1,OUTPUT);
+ pinMode(dir2,OUTPUT);
+  pinMode(pwm1,OUTPUT);
+
+ 
+}
+void loop()
+{
+   //digitalWrite(dir1,0);
+  digitalWrite(dir2,1);
+   analogWrite(pwm1,255);
+}
+
